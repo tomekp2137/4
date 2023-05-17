47af12cf8e28f6372bdbe3c897eb348aa30dd5eb
tomekp2137 <127328323+tomekp2137@users.noreply.github.com>
2023-05-17 14:37:32 +0000

    4_1

--- Main.java
+++ Main.java
@@ -6,17 +6,22 @@ Kod bazowy programu Commit4_0:
 • Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
 • Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
 */
-
+import java.util.Scanner;
 import java.io.IOException;
 
 class Main {
   public static void main(String[] args) {
-
-
-    
-    switch(wybor)
-      
-      try {
+    Scanner scan = new Scanner(System.in);
+int wybor;
+    System.out.println("1-zamknij program, 2- wypisz studentow, 3-dopisz studentow" );
+  
+wybor=scan.nextInt();
+    switch(wybor){
+      case 1:
+        System.exit(0);
+        
+      case 2:
+       try {
       Service s = new Service();
       s.addStudent(new Student("Krzysztof", 20));
       s.addStudent(new Student("Janusz", 40));
@@ -25,9 +30,9 @@ class Main {
       for(Student current : students) {
         System.out.println(current.ToString());
       }
-    } catch (IOException e) {
-
-    }
-      }
+    } catch (IOException e) {}
+      case 3:
+      
+   }
   }
 }
