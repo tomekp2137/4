80b54a19033b104b92de2075de3d8a1b0da22c74
tomekp2137 <127328323+tomekp2137@users.noreply.github.com>
2023-05-17 14:20:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+/*
+Kod bazowy programu Commit4_0: 
+• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
+• Studenci dodawani są w klasie Main.
+• Wszyscy studenci są wypisywani na końcu klasy Main.
+• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
+• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
+*/
+
+import java.io.IOException;
+
+class Main {
+  public static void main(String[] args) {
+
+
+    
+    switch(wybor)
+      
+      try {
+      Service s = new Service();
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+
+    }
+      }
+  }
+}
